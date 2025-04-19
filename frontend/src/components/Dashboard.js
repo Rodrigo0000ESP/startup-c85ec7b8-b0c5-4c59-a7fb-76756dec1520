dd0a62737c83f78c4d7d4ff447fd08ac5d97298e
Rodrigo0000ESP <cordejavihd@gmail.com>
2025-04-19 19:57:23 +0100

    Delete frontend directory

--- frontend/src/components/Dashboard.js
+++
@@ -1,5 +0,0 @@
-const aggregatedData = receiveDataFromMultipleSources();
-const analyzedData = passDataToAIAlgorithms(aggregatedData);
-const visualizations = generateVisualizations(analyzedData);
-const decisionRecommendations = customizeDecisionRecommendations(visualizations);
-displayOnDashboard(decisionRecommendations);
